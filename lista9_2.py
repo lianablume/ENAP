952c259609b49089d3a8e814b9e997a730c9b7ad
lianablume <lianablume@gmail.com>
2024-11-14 11:33:55 -0300

    Create lista9_2.py

---
+++ lista9_2.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Localização das comunidades quilombolas(2022)')
+df=pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+df=df.drop(columns=['Unnamed: 0'])
+#convertendo para numeros
+list = ['Lat_d', 'Long_d']
+df[list] = df[list].apply(pd.to_numeric,errors='coerce')
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+dadosFiltrados=df[df['NM_UF']==estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
+st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
+qtdeMunicipios=len(df['NM_MUNIC'].unique())
+st.write("A quantidade de municípios com localização quilombola é "+str(qtdeMunicipios))
+
+qtdeComunidades = len(df['NM_AGLOM'].unique())
+st.write("A quantidade de comunidades quilombolas é +str(qtdeComunidades)")
+st.header('Número de comunidades por UF')
+st.bar_chart(df['NM_UF'].value_counts())
